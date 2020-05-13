386c7a5e04a7dd41dd18083975c314510fc4b597
KeithorKeith <keithnimmo@hotmail.co.uk>
2020-05-13 18:42:31 +0100

    added station

--- Homework_day3.rb
+++ Homework_day3.rb
@@ -1,5 +1,7 @@
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 
-    stops[5] = "waverly"
-
+    stops[5] = "Edinburgh Waverly"
+    stops.shift()
+        p stops
+        stops.unshift("Glasgow Queen Street")
 p stops
